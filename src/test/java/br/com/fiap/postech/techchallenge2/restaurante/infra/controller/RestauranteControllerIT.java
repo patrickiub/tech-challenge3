5e153ed6080ce37fd51e29b1b39fb1667d2b9d05
Patrick N A <patrickdevtech@gmail.com>
2026-05-10 17:25:11 -0300

    fix(tests): add @WithMockUser to ControllerIT tests to bypass Spring Security

--- src/test/java/br/com/fiap/postech/techchallenge2/restaurante/infra/controller/RestauranteControllerIT.java
+++ src/test/java/br/com/fiap/postech/techchallenge2/restaurante/infra/controller/RestauranteControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(roles = "CLIENTE")
 class RestauranteControllerIT {
 
     @Autowired
